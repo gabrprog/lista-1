7dee122a3a8becdc31fba1ce2f6152b23bddd5b4
gabrprog <g.alberto.dev@gmail.com>
2024-03-25 09:08:32 -0300

    Exercício 1

---
+++ Exec01.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exec01 {
+    public static void executar() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número: ");
+        int numero = scanner.nextInt();
+
+        if (numero > 10) {
+            System.out.println("Número maior do que 10!");
+        } else {
+            System.out.println("Número menor ou igual a 10!");
+        }
+    }
+}
