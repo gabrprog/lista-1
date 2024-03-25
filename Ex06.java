8da1fafdc7fd4059b1c36136615f4d580c844623
gabrprog <g.alberto.dev@gmail.com>
2024-03-25 10:14:29 -0300

    Exercício 6

--- Ex06.java
+++ Ex06.java
@@ -1,3 +1,16 @@
-public class Ex06 {
+import java.util.Scanner;
 
+public class Ex06 {
+    public static void main() {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura em graus Celsius: ");
+        double celsius = leitor.nextDouble();
+
+        double fahrenheit = (9 * celsius + 160) / 5;
+
+        System.out.println("A temperatura em graus Fahrenheit é: " + fahrenheit);
+
+        
+    }
 }
