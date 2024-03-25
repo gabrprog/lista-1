c4ece5755c622cf28d4f44e757113b18539aa17b
gabrprog <g.alberto.dev@gmail.com>
2024-03-25 09:45:51 -0300

    Exercício 4

--- Ex04.java
+++ Ex04.java
@@ -1,3 +1,25 @@
-public class Ex04 {
+import java.util.Scanner;
 
+public class Ex04 {
+    public static void executar() {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número: ");
+        double numero1 = leitor.nextDouble();
+
+        System.out.println("Digite o segundo número: ");
+        double numero2 = leitor.nextDouble();
+
+        double soma = numero1 + numero2;
+        double subtracao = numero1 - numero2;
+        double multiplicacao = numero1 * numero2;
+        double divisao = numero1 / numero2;
+
+        System.out.println("Soma: " + soma);
+        System.out.println("Subtração: " + subtracao);
+        System.out.println("Multiplicação: " + multiplicacao);
+        System.out.println("Divisão: " + divisao);
+
+        
+    }
 }
--- Programa.java
+++ Programa.java
@@ -2,7 +2,8 @@ public class Programa {
     public static void main(String[] args) {
         // Exec01.executar();
         //Ex02.executar();
-        Ex03.executar();
-        
+        //Ex03.executar();
+        Ex04.executar();
+
     }
 }
